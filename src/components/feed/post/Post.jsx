4057c5b6cfc42e44038cd9f96f5ab9a98e73d2ac
yayaveli <yabadji2010@gmail.com>
2021-10-19 18:17:40 +0000

    Add Post component -> design only

---
+++ src/components/feed/post/Post.jsx
@@ -0,0 +1,39 @@
+import { MoreVert } from '@mui/icons-material'
+import './post.css'
+const Post = () => {
+  return (
+    <div className="post">
+      <div className="postWrapper">
+        <div className="postTop">
+          <div className="postTopLeft">
+            <img
+              src="/assets/person/1.jpeg"
+              alt=""
+              className="postProfileImg"
+            />
+            <span className="postUsername">Yaya SANE</span>
+            <span className="postDate">5 mins ago</span>
+          </div>
+          <div className="postTopRight">
+            <MoreVert />
+          </div>
+        </div>
+        <div className="postCenter"></div>
+        <span className="postText">Hey! Its my first post :)</span>
+        <img className="postImg" src="/assets/post/1.jpeg" alt="" />
+        <div className="postBottom">
+          <div className="postBottomLeft">
+            <img className="likeIcon" src="/assets/like.png" alt="" />
+            <img className="likeIcon" src="/assets/heart.png" alt="" />
+            <span className="postLikeCounter">32 people like it</span>
+          </div>
+          <div className="postBottomRight">
+            <span className="postCommentText">9 comments</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Post
