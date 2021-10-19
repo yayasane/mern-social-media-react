60354a48bb63fc0bfa07d943f9ca1dc9c3cd952a
yayaveli <yabadji2010@gmail.com>
2021-10-19 20:53:53 +0000

    Add like & unlike Post feature

--- src/components/feed/post/Post.jsx
+++ src/components/feed/post/Post.jsx
@@ -1,8 +1,16 @@
 import { MoreVert } from '@mui/icons-material'
 import './post.css'
 import { Users } from '../../../dummyData'
+import { useState } from 'react'
 const Post = ({ post }) => {
   const user = Users.filter((u) => u.id === post.userId)
+  const [like, setLike] = useState(post.like)
+  const [isLiked, setIsLiked] = useState(false)
+
+  const likeHandler = () => {
+    setLike((l) => (isLiked ? l - 1 : l + 1))
+    setIsLiked((l) => !l)
+  }
   return (
     <div className="post">
       <div className="postWrapper">
@@ -25,9 +33,19 @@ const Post = ({ post }) => {
         <img className="postImg" src={post.photo} alt="" />
         <div className="postBottom">
           <div className="postBottomLeft">
-            <img className="likeIcon" src="/assets/like.png" alt="" />
-            <img className="likeIcon" src="/assets/heart.png" alt="" />
-            <span className="postLikeCounter">{post.like} people like it</span>
+            <img
+              className="likeIcon"
+              src="/assets/like.png"
+              alt=""
+              onClick={likeHandler}
+            />
+            <img
+              className="likeIcon"
+              src="/assets/heart.png"
+              alt=""
+              onClick={likeHandler}
+            />
+            <span className="postLikeCounter">{like} people like it</span>
           </div>
           <div className="postBottomRight">
             <span className="postCommentText">{post.comment} comments</span>
