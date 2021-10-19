65a1ab9fce9d23aa436d0cb1d59ed76f2a664563
yayaveli <yabadji2010@gmail.com>
2021-10-19 17:06:10 +0000

    Add Feed Component && Share Component -> design only

--- src/components/feed/Feed.jsx
+++ src/components/feed/Feed.jsx
@@ -1,6 +1,13 @@
 import './feed.css'
+import Share from './share/Share'
 const Feed = () => {
-  return <div className="feed">feed</div>
+  return (
+    <div className="feed">
+      <div className="feedWrapper">
+        <Share />
+      </div>
+    </div>
+  )
 }
 
 export default Feed
